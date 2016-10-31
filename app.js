707ac586acf9a70b2d9098f3aa78a365ca338e1c
Joseph Tongay <dirk.tongay@gmail.com>
2016-10-31 13:50:25 -0500

    initial tests are passing with repeater

---
+++ app.js
@@ -0,0 +1,53 @@
+var app = angular.module( 'reddit', [] )
+
+app.controller( 'frontPage', [ '$scope', ( $scope ) => {
+    $scope.view = {};
+    $scope.view.welcome = 'Welcome to the Front Page';
+
+    $scope.view.posts = [ {
+        title: 'Check this out!',
+        author: 'Gargoyle',
+        description: 'I made this place in Angular!! Isn\'t it cool??',
+        imageURL: 'https://media-cdn.tripadvisor.com/media/photo-s/03/9b/30/26/seattle.jpg',
+        votes: 6,
+        date: new Date( 'October 30, 2016 02:53:00' ),
+        comments: [ {
+            author: 'Richard',
+            text: 'Yea, so what? I used React and it was way easier.'
+        }, {
+            author: 'Linus',
+            text: 'Vue is better....'
+        }, {
+            author: 'Peter',
+            text: 'Yea, but did you write tests for it?'
+        } ]
+    }, {
+      title: 'This place has all the cool stuff.',
+      author: 'Linus',
+      description: 'Really though, I enjoy the weather and architecture.',
+      imageURL: 'http://i.huffpost.com/gen/1470163/images/o-ALBUQUERQUE-SKYLINE-facebook.jpg',
+      votes: 2,
+      date: new Date( 'October 13, 2016 23:12:00' ),
+      comments: [ {
+          author: 'Gargoyle',
+          text: 'It\'s too hot for me there....'
+      }, {
+          author: 'Linus',
+          text: 'Get over it!'
+      } ]
+    }, {
+      title: 'They say home is where the heart is.',
+      author: 'Peter',
+      description: 'And this place is lovely.',
+      imageURL: 'https://cache-graphicslib.viator.com/graphicslib/thumbs360x240/2540/SITours/san-diego-champagne-brunch-cruise-in-san-diego-101219.jpg',
+      votes: -5,
+      date: new Date( 'October 24, 2016 10:23:00' ),
+      comments: [ {
+          author: 'Gargoyle',
+          text: 'San Diego is for losers dude.'
+      }, {
+          author: 'Linus',
+          text: 'Yeah, only hipsters and nerds go there.'
+      } ]
+    } ]
+} ] )
---
+++ spec/reddit-test.js
@@ -0,0 +1,31 @@
+describe('Reddit Test FTW', function () {
+
+  beforeEach(function () {
+    browser.get('http://localhost:8000');
+  })
+
+
+  it('should pass the test', function () {
+    expect(true).toBe(true);
+  })
+
+  it('should welcome the user to Reddit', function () {
+    expect(element.all(by.tagName('h1')).get(0).getText()).toBe('Welcome to the Front Page');
+  })
+
+  it('should show the title of all of the posts', function () {
+    expect(element.all(by.repeater('post in view.posts').row(0)).getText()).toMatch('Check this out!');
+    expect(element.all(by.repeater('post in view.posts').row(1)).getText()).toMatch('This place has all the cool stuff.');
+    expect(element.all(by.repeater('post in view.posts').row(2)).getText()).toMatch('They say home is where the heart is.');
+
+  })
+
+  it('should show the description of all of the posts', function () {
+    expect(element.all(by.repeater('post in view.posts').row(0)).getText()).toMatch('I made this place in Angular!! Isn\'t it cool??');
+    expect(element.all(by.repeater('post in view.posts').row(1)).getText()).toMatch('Really though, I enjoy the weather and architecture.');
+    expect(element.all(by.repeater('post in view.posts').row(2)).getText()).toMatch('And this place is lovely.');
+
+  })
+
+
+})
